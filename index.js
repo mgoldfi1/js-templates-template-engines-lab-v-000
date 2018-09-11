ff502dced910364e3892d5462dc1552fd58045aa
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-11 15:32:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ function createPost() {
 
     var postTemplate = document.getElementById("post-template").innerHTML
     var templateFn = _.template(postTemplate)
-    var postsDiv1 = document.getElementById("post-title");
-    var postsDiv2 = document.getElementById("post-author")
-    var templateHTML1 = templateFn({ 'title': title});
-    var templateHTML2 = templateFn({'author': author })
-    postsDiv1.innerHTML += templateHTML1;
-    postsDiv2.innerHTML += templateHTML2;
+    var postsDiv = document.getElementById("posts");
+    var templateHTML = templateFn({ 'title': title, 'author': author });
+    postsDiv.innerHTML += templateHTML;
 
 
 }
