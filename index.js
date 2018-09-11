f023e17b29258ca1941c75150cf060f981080790
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-11 14:36:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function createPost() {
-
+    var title = document.getElementById("title").value
+    var author = document.getElementById("author").value
+    var body = document.getElementById("body").value
 }
 
 
