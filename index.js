050ad727e5737d044de7ec1ac3ea843e3961067c
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-11 14:53:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function createPost() {
-    event.preventDefault()
     var title = document.getElementById("title").value
     var author = document.getElementById("author").value
     var body = document.getElementById("body").value
