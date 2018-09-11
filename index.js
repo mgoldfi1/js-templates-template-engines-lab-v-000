4e09b5fe631e04eedf6f7c621b880d5e13c5f9f5
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-11 15:37:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function createPost() {
     var title = document.getElementById("title").value
     var author = document.getElementById("author").value
     var body = document.getElementById("body").value
-
+    console.log(title)
     var postTemplate = document.getElementById("post-template").innerHTML
     var templateFn = _.template(postTemplate)
     var postsDiv = document.getElementById("post-template");
